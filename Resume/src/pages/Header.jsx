c1bd0671c1f27601b17251cccac3bd0a1c8ec74f
Shubham Naiknaware <62801488+shubhamnaiknaware@users.noreply.github.com>
2024-04-13 18:17:12 +0530

    This is a boiler plate for vite +  react and tailwind css

---
+++ Resume/src/pages/Header.jsx
@@ -0,0 +1,24 @@
+const Logo = () => {
+  return (
+    <div>
+      <img src="" alt="" className="" />
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <Logo />
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Resume</li>
+        <li>Hire Me</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
---
+++ Resume/src/pages/Project.jsx
@@ -0,0 +1,9 @@
+const Project = () => {
+  return (
+    <div>
+      <h1>This is a Projects section</h1>
+    </div>
+  );
+};
+
+export default Project;
